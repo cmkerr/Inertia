705d5bbd971cc86d2be020026b2fca8b7cae1877
cmkerr <cmkerr@gmail.com>
2013-06-02 22:16:39 -0600

    Stubbing out some of the initial directory structure and submitting some placeholder files for classes I know I'll be implementing.

---
+++ Engine/Main/Main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/**
+	This class is the platform-independent main class for the engine. It
+	handles initialization of all top-level systems and contains the main
+	update loop.
+*/
+
+
+class Main {
+
+
+
+
+
+};
---
+++ Engine/Main/OSX/OSXMain.cpp
@@ -0,0 +1,14 @@
+/**
+	This is the platform-specific main for Mac OSX. This file should contain the actual main() 
+	function and creation of the window and messaging callbacks.
+
+*/
+
+
+int main(int argc, char **argv)
+{
+	
+	
+	
+	
+}
