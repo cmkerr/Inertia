0aa26531fd268a3f91697bf6febb061ca4e2d14e
cmkerr <cmkerr@gmail.com>
2013-06-03 22:09:48 -0600

    Setting the loop() function of JobThread to private. Setting std::thread as a friend of JobThread so it can access loop().

--- Engine/Job/JobThread.h
+++ Engine/Job/JobThread.h
@@ -9,14 +9,15 @@ class JobThread
 private:
 	bool m_running;
 	std::thread *m_thread;
-
 	JobManager *m_manager;
 
+    // This allows the loop function to be set private
+    friend class std::thread;
+    void loop();
+    
 public:
 	JobThread(JobManager *);
 	~JobThread(void);
-
-	void loop();
 
 	void shutdown();
 };
