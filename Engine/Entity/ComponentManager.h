e8e44eb1a83da2757896446633ca684467c19bbb
cmkerr <cmkerr@gmail.com>
2013-06-12 19:55:37 -0600

    Replacing the underlying data structure of ComponentManager with a btree. This should provide better performance when iterating over components.

--- Engine/Entity/ComponentManager.h
+++ Engine/Entity/ComponentManager.h
@@ -3,13 +3,12 @@
 #include "Entity.h"
 #include "Component.h"
 
-#include <map>
-
+#include "External/btree/btree_map.h"
 
 template <class T>
 class ComponentManager {
 private:
-    std::map<Entity*, T*> m_components;
+    btree::btree_map<Entity*, T*> m_components;
     
     static ComponentManager<T> s_manager;
 public:
@@ -26,7 +25,7 @@ public:
     
     void removeComponent(T* component)
     {
-        typename std::map<Entity*, T*>::iterator it = m_components.begin();
+        typename btree::btree_map<Entity*, T*>::iterator it = m_components.begin();
         for (; it != m_components.end(); ++it)
         {
             if (it->second == component)
@@ -38,19 +37,19 @@ public:
     }
     
     
-    class iterator : public std::map<Entity*, T*>::iterator {
+    class iterator : public btree::btree_map<Entity*, T*>::iterator {
     private:
-        iterator(typename std::map<Entity*, T*>::iterator it) : std::map<Entity*, T*>::iterator(it)
+        iterator(typename btree::btree_map<Entity*, T*>::iterator it) : btree::btree_map<Entity*, T*>::iterator(it)
         {
-            entity = ((typename std::map<Entity*, T*>::iterator) *this)->first;
-            component = ((typename std::map<Entity*, T*>::iterator) *this)->second;
+            entity = ((typename btree::btree_map<Entity*, T*>::iterator) *this)->first;
+            component = ((typename btree::btree_map<Entity*, T*>::iterator) *this)->second;
         }
         friend ComponentManager<T>;
     public:
         Entity *entity;
         T *component;
         
-        iterator() : std::map<Entity*, T*>::iterator()
+        iterator() : btree::btree_map<Entity*, T*>::iterator()
         {
             //entity =
             entity = nullptr;
@@ -59,20 +58,20 @@ public:
 
         iterator& operator++()
         {
-            std::map<Entity*, T*>::iterator::operator++();
+            btree::btree_map<Entity*, T*>::iterator::operator++();
 
-            entity = ((typename std::map<Entity*, T*>::iterator) *this)->first;
-            component = ((typename std::map<Entity*, T*>::iterator) *this)->second;
+            entity = ((typename btree::btree_map<Entity*, T*>::iterator) *this)->first;
+            component = ((typename btree::btree_map<Entity*, T*>::iterator) *this)->second;
             
             return *this;
         }
         iterator operator++(int)
         {
             iterator ret = *this;
-            std::map<Entity*, T*>::iterator::operator++();
+            btree::btree_map<Entity*, T*>::iterator::operator++();
             
-            entity = ((typename std::map<Entity*, T*>::iterator) *this)->first;
-            component = ((typename std::map<Entity*, T*>::iterator) *this)->second;
+            entity = ((typename btree::btree_map<Entity*, T*>::iterator) *this)->first;
+            component = ((typename btree::btree_map<Entity*, T*>::iterator) *this)->second;
             
             return ret;
         }
@@ -95,111 +94,3 @@ public:
 template <class T>
 ComponentManager<T> ComponentManager<T>::s_manager;
 
-/*
-template <class T>
-class ComponentManager {
-private:
-    static ComponentManager<T> *s_manager;
-    
-    std::map<Entity, T*> m_components;
-    
-public:
-    class iterator;
-    
-    void addComponent(Entity& entity, T* component)
-    {
-        m_components.insert(std::pair<Entity, T*>(entity, component ));
-    }
-    
-    void removeComponent(Entity& entity)
-    {
-        m_components.erase(entity);
-    }
-    
-    T* getComponent(Entity& e)
-    {
-        T* comp = nullptr;
-        
-        typename std::map<Entity, T*>::iterator it = m_components.find(e);
-        if (it != m_components.end())
-            comp = it->second;
-        
-        return comp;
-    }
-    
-    
-    ComponentManager<T>::iterator begin()
-    {
-        ComponentManager<T>::iterator it;
-        it.it = m_components.begin();
-        
-        it.entity = it.it->first;
-        it.component = it.it->second;
-        
-        return it;
-    }
-    
-    ComponentManager<T>::iterator end()
-    {
-        ComponentManager<T>::iterator it;
-        it.it = m_components.end();
-        
-        it.entity = it.it->first;
-        it.component = it.it->second;
-        
-        return it;
-    }
-    
-    static ComponentManager<T>* Instance()
-    {
-        if (!s_manager)
-            s_manager = new ComponentManager<T>();
-        
-        return s_manager;
-    }
-    
-    class iterator {
-    private:
-        typename std::map<Entity, T*>::iterator it;
-    public:
-        Entity entity;
-        T* component;
-        
-        friend bool operator==(const ComponentManager<T>::iterator& i1, const ComponentManager<T>::iterator& i2);// {return i1.it == i2.it; }
-        friend bool operator!=(const ComponentManager<T>::iterator& i1, const ComponentManager<T>::iterator& i2);// {return i1.it != i2.it; }
-        
-        iterator& operator++()
-        {
-            ++it;
-            
-            entity = it->first;
-            component = it->second;
-            
-            return *this;
-        }
-        
-        iterator operator++(int)
-        {
-            iterator old = *this;
-            
-            ++it;
-            
-            entity = it->first;
-            component = it->second;
-            
-            return old;
-        }
-        
-        friend class ComponentManager;
-    };
-};
-
-template <class T>
-bool operator==(const typename ComponentManager<T>::iterator& i1, const typename ComponentManager<T>::iterator& i2) {return i1.it == i2.it; }
-
-template <class T>
-bool operator!=(const typename ComponentManager<T>::iterator& i1, const typename ComponentManager<T>::iterator& i2) {return i1.it != i2.it; }
-
-template <class T>
-ComponentManager<T>* ComponentManager<T>::s_manager = nullptr;
-*/
